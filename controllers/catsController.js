aaaa6f0ef58ea8e7a81767a7e58cd2a57e21798b
Perri Specter <plspecter@gmail.com>
2018-11-06 20:27:24 -0500

    sleepyCats-1

--- controllers/catsController.js
+++ controllers/catsController.js
@@ -8,6 +8,9 @@ var express = require("express");
 
 var router = express.Router();
 
+// Import the model (cat.js) to use its database functions.
+var cat = require("../models/cat.js");
+
 //What we are responding to the client with when they send requests to our server
 //the router is getting all of the html requests ("/") and responding when the user..
 //requests anything
@@ -29,15 +32,45 @@ router.get("/", function (req, res) {
 router.post("api/cats", function (req, res) {
     cat.create([
         "name", "sleepy"]), [req.body.name, req.body.sleepy], function
-    (result) {
-        //send back the id of the new quote (new cat)
-        res.json({id: result.insertId});
-    }
-res.sendFile(path.join(__dirname, ""))
+            (result) {
+            //send back the id of the new quote (new cat)
+            res.json({ id: result.insertId });
+        }
+    res.sendFile(path.join(__dirname, ""))
 });
 
 //WE CAN UPDATE A CAT
 router.put("/api/cats/:id", function (req, res) {
-var consition = "id = " + req.params.id;
-console.log("condition", condition)
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition)
+
+
+    cat.update({
+        sleepy: req.body.sleepy
+    }), condition, function (result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    }
+
 })
+
+//WE CAN DELETE A CAT
+router.delete("/api/cats/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    cat.delete(condition, function (results) {
+        if (results.affectedRows == 0) {
+            return res.status(400).end()
+        } else {
+            res.status(200).end();
+        }
+    })
+})
+
+//export the routes for server.js to use
+module.exports = router;
+
+
